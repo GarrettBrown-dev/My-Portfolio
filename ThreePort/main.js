40cb7adfbd53cdd83e74fb46565ab41198c07f54
Garrett <gman9mm@live.com>
2021-06-16 14:55:51 -0700

    Change header text.

--- ThreePort/main.js
+++ ThreePort/main.js
@@ -36,9 +36,9 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+//const lightHelper = new THREE.PointLightHelper(pointLight);
+//const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
