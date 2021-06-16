fff7bd81bf370bbcaf801c8befe998cd6544f6cb
Garrett <gman9mm@live.com>
2021-06-16 13:30:13 -0700

    Add shape and animation function.

--- ThreePort/main.js
+++ ThreePort/main.js
@@ -19,3 +19,19 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6347,
+  wireframe: true,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
